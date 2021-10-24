3bb0f866757e6ba2c03504a1b1d0466380be135c
nikudango <satoru_rutosa@icloud.com>
2021-10-25 01:30:21 +0900

    1st commit

--- test.js
+++ test.js
@@ -1,12 +1,40 @@
+// const puppeteer = require('puppeteer');
+
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto('https://coconala.com/search?keyword=3d%20vtuber');
+//   const tables = await page.$$("title")
+//   console.log(tables);
+
+//   await browser.close();
+// })();
+
+const { title } = require('process');
 const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://coconala.com/search?keyword=3d%20vtuber', {
-    waitUntil: 'networkidle2',
-  });
-  await page.pdf({ path: 'hn.pdf', format: 'a4' });
+  let n = 1;
+  while (n<2) {
+    await page.goto('https://coconala.com/search?keyword=3d%20vtuber'+'&business_flag=false&page='+n,{
+      waitUntil: 'networkidle2'
+    });
+    const tables = await page.$$("a");
+    const d = await Promise.all(tables.map(async table => {
+        const t = (await (await table.getProperty("textContent")).jsonValue()).trim();
+        const ts = t.split("\n");
+        return {
+            table: t,
+        }
+    }))
+  
+    console.log(d);
+    n++;
+  }
+  
+  
 
   await browser.close();
 })();
