d26da4a12c5ddd41eaef4636bf260ae8c380495f
nikudango <satoru_rutosa@icloud.com>
2021-10-25 22:37:50 +0900

    get title and price

--- test.js
+++ test.js
@@ -10,31 +10,61 @@
 //   await browser.close();
 // })();
 
-const { title } = require('process');
+
+
+// const { title } = require('process');
+// const puppeteer = require('puppeteer');
+
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   let n = 1;
+//   while (n<2) {
+//     await page.goto('https://coconala.com/search?keyword=3d%20vtuber'+'&business_flag=false&page='+n,{
+//       waitUntil: 'networkidle0'
+//     });
+//     const tables = await page.$$("p");
+//     const d = await Promise.all(tables.map(async table => {
+//         const t = (await (await table.getProperty("textContent")).jsonValue()).trim();
+//         const ts = t.split("\n");
+//         return {
+//             table: t
+//         }
+//     }))
+//     console.log(d);
+//     n++;
+//   }
+//   await browser.close();
+// })();
+
+
+
+
+
+
 const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  
+  const target = 'p'
+  const prices = 'strong'
   let n = 1;
-  while (n<2) {
-    await page.goto('https://coconala.com/search?keyword=3d%20vtuber'+'&business_flag=false&page='+n,{
-      waitUntil: 'networkidle2'
-    });
-    const tables = await page.$$("a");
-    const d = await Promise.all(tables.map(async table => {
-        const t = (await (await table.getProperty("textContent")).jsonValue()).trim();
-        const ts = t.split("\n");
-        return {
-            table: t,
-        }
-    }))
-  
-    console.log(d);
-    n++;
-  }
-  
-  
+    while (n<=2) {
+      await page.goto('https://coconala.com/search?keyword=3d%20vtuber'+'&business_flag=false&page='+n,{
+        waitUntil: 'networkidle0'
+      });
+      const title = await page.$$eval(target, elements => {
+        return elements.map(element => element.textContent.trim())    // 無駄なスペースを減らしてセット
+      });
+      const price = await page.$$eval(prices, elements => {
+        return elements.map(element => element.textContent.trim())    // 無駄なスペースを減らしてセット
+      });
+      console.log(title);
+      console.log(price);
+      n++;
+    }
 
   await browser.close();
 })();
