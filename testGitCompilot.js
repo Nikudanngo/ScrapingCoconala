ab66a0626cdbe80404721238a68213f56bb7a595
nikudango <satoru_rutosa@icloud.com>
2021-10-28 00:44:51 +0900

    add testcompilot

---
+++ testGitCompilot.js
@@ -0,0 +1,7 @@
+function getDate() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    return day + "/" + month + "/" + year;
+}console.log(getDate());
