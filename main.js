706cbcba7cdcc5a0f4e632c751fd12db935b5d01
Nikudanngo <satoru_rutosa@icloud.com>
2021-10-27 15:49:18 +0900

    acync

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const puppeteer = require('puppeteer');
-
+const Promise=require('promise');
+let data=[];
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   
   const target = 'p'
-  const prices = 'strong'
+  const prices = '.c-searchItemContentPrice_price'
 
   pageUrl = 'https://coconala.com/search?keyword=3d%20vtuber&layout=2&ref_c=1&y=0&sort_by=ranking&business_flag=false&page='
   
@@ -20,11 +21,23 @@ const puppeteer = require('puppeteer');
       const price = await page.$$eval(prices, elements => {
         return elements.map(element => element.textContent.trim())    
       });      
-      // console.log(title);
-      for(let i=0;i<price.length;i++) {
-        console.log(price[i]);
-      }
+      // // console.log(title);
+      // for(let i=0;i<price.length;i++) {
+      //   console.log(price[i]);
+      // }
+      const promise=()=>{
+        return new Promise((resolve)=>{
+        price.map((e)=>{
+          data.push(e);
+        })
+      resolve();
+      })
+      
+    }
+    await promise();
+    console.log(data);
       n++;
     }
   await browser.close();
+  
 })();
