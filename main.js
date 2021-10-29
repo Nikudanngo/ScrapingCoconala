1a99b15349ae496d9d681585cc2d8c618c101728
nikudango <satoru_rutosa@icloud.com>
2021-10-30 02:10:16 +0900

    add coment by compilot

--- main.js
+++ main.js
@@ -6,48 +6,53 @@ let allData=[];
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   
-  const target = 'p'
-  const prices = '.c-searchItemContentPrice_price'
-
-  pageUrl = 'https://coconala.com/search?keyword=3d%20vtuber&layout=2&ref_c=1&y=0&sort_by=ranking&business_flag=false&page='
+  const keyWord = '3d%20vtuber'  //検索ワード
+  const target = 'p'  //検索対象
+  const ratings = '.c-searchItemContentPriceRating_count' //評価数
+  const prices = '.c-searchItemContentPrice_price'  //価格
+  pageUrl = 'https://coconala.com/search?keyword=' + keyWord + '&layout=2&ref_c=1&y=0&sort_by=ranking&business_flag=false&page='  //ページURL
   
-  let n = 1;
+  let n = 5;
   const promise=()=>{
-    return new Promise(async(resolve)=>{
-    while (n<=2) {
-      await page.goto(pageUrl+n,{
-        waitUntil: 'networkidle0'
+    return new Promise(async(resolve)=>{  
+    while (n<=6) {      
+      await page.goto(pageUrl+n,{ 
+        waitUntil: 'networkidle0'   //ページが読み込まれるまで待機
       });
-      const title = await page.$$eval(target, elements => {
-        return elements.map(element => element.textContent.trim())
+      const title = await page.$$eval(target, elements => {   //タイトル
+        return elements.map(element => element.textContent.trim())  //検索結果のタイトルを取得
       });
       const price = await page.$$eval(prices, elements => {
         return elements.map(element => element.textContent.trim())    
-      });      
+      }); 
+      const rating = await page.$$eval(ratings, elements => {
+        return elements.map(element => element.textContent.trim())    
+      });    
       // // console.log(title);
       // for(let i=0;i<price.length;i++) {
       //   console.log(price[i]);
       // }
-        price.map((e,index)=>{
-          data.push(e);
+        price.map((e,index)=>{  //価格を数値に変換
+          data.push(e);     //価格を配列に格納
         })
 
-        if(data.length<=0) {
-          break;
+        if(data.length<=0) {  //価格が取得できなかった場合
+          break;  //ループを抜ける
         }
 
         console.log(n+":OK");
-        data.map((e,index)=>{
+        data.map((e,index)=>{   //価格を数値に変換
 
-          allData.push({
-            name:"title"+index,
-            price:e
+          allData.push({  //配列に格納
+            title:title[index],  // Github Compilotの提案でなぜかタイトル取れたすげぇ
+            rating:rating[index], //評価数
+            price:price[index]  //価格
           });
         })
         n++;
         
       }
-      resolve();
+      resolve();  //resolveでPromiseを終了させる
   })
 }
     await promise();
