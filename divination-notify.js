eb0a14b56b6f731427ee3d81d7ed0e752ebd0dd1
nikudango <satoru_rutosa@icloud.com>
2021-11-13 23:41:22 +0900

    can tweet

--- divination-notify.js
+++ divination-notify.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 const Promise=require('promise');
 const fs = require('fs');
+const setting = require('./testTweet');
+const os = require('os');
 
 let data=[{
     "スッキリ":0,
@@ -20,7 +22,7 @@ let data=[{
     const AuUrl = 'https://fortune.auone.jp/'
     const MezamasiUrl = 'https://www.fujitv.co.jp/meza/uranai/index.html'
     const UranaiNabiUrl = 'https://p.dwdw.net/official/dailySignRanking/index.html'
-    const GoisuNetUrl = 'https://goisu.net/daily/'
+    // const GoisuNetUrl = 'https://goisu.net/daily/'
 
 
     // 各ページのclass要素
@@ -29,7 +31,7 @@ let data=[{
     const au = '.ft-mainbox__stars_img'    //auの星座
     const mezamasi = '.result1 p '    //目覚ましの星座占い
     const uranainabi = '.today_ranking a'    //うらないなびの星座
-    const goisu = 'h3'    //ゴイスネットの星座
+    // const goisu = 'h3'    //ゴイスネットの星座   あとでよいスクレイピング方法を考える
 
     await page.goto(SukkiriUrl,{
         waitUntil: 'networkidle0'
@@ -61,20 +63,28 @@ let data=[{
     const UranaiRank = await page.$$eval(uranainabi, els => els.map(el => el.innerText));    //検索対象の◯月かを取得
     console.log("ok");
 
-    await page.goto(GoisuNetUrl,{
-        waitUntil: 'networkidle0'
-    });
-    const GoisuNetRank = await page.$$eval(goisu, els => els.map(el => el.innerText));    //検索対象の◯月かを取得
-    console.log("ok");
+    // await page.goto(GoisuNetUrl,{
+    //     waitUntil: 'networkidle0'
+    // });
+    // const GoisuNetRank = await page.$$eval(goisu, els => els.map(el => el.innerText));    //検索対象の◯月かを取得
+    // console.log("ok");
     
     data[0].スッキリ = SRank[SRank.length-1];   //スッキリでの1位をdataに格納
     data[0].朝日 = ARank[ARank.length-1];   //朝日での1位をdataに格納
     data[0].au = AuRank[AuRank.length-1].trim();   //auでの1位をdataに格納.トリミングして余分な空白を削除
     data[0].めざまし = MeRnak[MeRnak.length-3];   //めざましでの1位をdataに格納
     data[0].占いなび = UranaiRank[0];   //うらないなびでの1位をdataに格納
-    data[0].ゴイスネット = GoisuNetRank[0];   //ゴイスネットでの1位をdataに格納
-
+    // data[0].ゴイスネット = GoisuNetRank[0];   //ゴイスネットでの1位をdataに格納
     console.log(JSON.stringify(data,null,2)); //最新のデータを表示
+    const sentense = '今日の占い一位の誕生月or星座' + os.EOL 
+    +'スッキリ:'+ data[0].スッキリ+ '月生まれ' + os.EOL
+    +'朝日:'+ data[0].朝日 + os.EOL
+    +'au:'+ data[0].au + os.EOL
+    +'めざまし:'+ data[0].めざまし + os.EOL
+    +'占いなび:'+ data[0].占いなび + os.EOL
+    // +'ゴイスネット:'+ data[0].ゴイスネット + os.EOL
+    // console.log(sentense);
+    setting.tweetPost(sentense); //ツイート
     await browser.close();  //ブラウザを閉じる
     
   })();
