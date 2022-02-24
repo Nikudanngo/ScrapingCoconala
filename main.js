7df09ef887889c0ffa613f9be4e9c00ec12c5e2c
nikudango <satoru_rutosa@icloud.com>
2022-02-24 23:09:30 +0900

    ココナラのclass属性の変更に伴い使えなくなっていたのを修正

--- main.js
+++ main.js
@@ -1,93 +1,81 @@
 const puppeteer = require('puppeteer');
-const Promise=require('promise');
+const Promise = require('promise');
 const fs = require('fs');
 const csv = require('csv');
 
-const keyWord = 'スクレイピング' //検索ワード
-let data=[]; 
-let allData=[];
+const keyWord = 'webサイト' //検索ワード
+let data = [];
+let allData = [];
 // let firstData=[keyWord,'price','rating'];
 
 
-(async () => { 
+(async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  
-  const target = '.c-searchItemContent_overview'  //検索対象
-  const prices = '.c-searchItemContentPrice_price'  //価格
-  const starratings = '.c-searchItemContentPrice_rating' //星と評価数
-  const urls = 'a'
+
+  const target = '.c-serviceBlockItemContent_name'  //サービスタイトル
+  const prices = '.c-serviceBlockItemContentPrice_price'  //価格
+  const starratings = '.c-serviceBlockItemContentPriceRating_count' //星と評価数
+  const urls = '.c-searchPage_itemList a' //サービスリンク
   pageUrl = 'https://coconala.com/search?keyword=' + keyWord + '&layout=2&ref_c=1&sort_by=ranking&business_flag=false&page='  //ページURL
 
 
   let n = 1;
-  const promise=()=>{
-    return new Promise(async(resolve)=>{
-    while (n<=1) {  
-      await page.goto(pageUrl+n,{ 
-        waitUntil: 'networkidle0'   //ページが読み込まれるまで待機
-      });
-      const title = await page.$$eval(target, elements => {   //タイトル
-        return elements.map(element => element.textContent.trim())  //検索結果のタイトルを取得
-      });
-      const price = await page.$$eval(prices, elements => {  //価格
-        return elements.map(element => element.textContent.trim())  //検索結果の価格を取得
-      }); 
-      const rating = await page.$$eval(starratings, elements => { //評価数
-        return elements.map(element => element.textContent.trim())  //検索結果の評価数を取得
-      });   
-      const url = await page.$$eval(urls, elements => { //評価数
-        return elements.map(element => element.textContent.href) //検索結果の評価数を取得
-      });    
+  const promise = () => {
+    return new Promise(async (resolve) => {
+      while (n <= 10) {
+        await page.goto(pageUrl + n, {
+          waitUntil: 'networkidle0'   //ページが読み込まれるまで待機
+        });
+        const title = await page.$$eval(target, elements => {   //タイトル
+          return elements.map(element => element.textContent.trim())  //検索結果のタイトルを取得
+        });
+        const price = await page.$$eval(prices, elements => {  //価格
+          return elements.map(element => element.textContent.trim())  //検索結果の価格を取得
+        });
+        const rating = await page.$$eval(starratings, elements => { //評価数
+          return elements.map(element => element.textContent.trim())  //検索結果の評価数を取得
+        });
+        // const url = await page.$$eval(urls, elements => { //URL
+        //   return elements.map(element => element.textContent.href) //サービスのURLを取得
+        // });
+        const url = await page.$$eval(urls, list => list.map(item => item.href)); //サービスのURLを取得
 
-
-        price.map((e,index)=>{  //価格を数値に変換
+        price.map((e, index) => {  //価格を数値に変換
           data.push(e);     //価格を配列に格納
         })
 
-        if(data.length<=0) {  //価格が取得できなかった場合
+        if (data.length <= 0) {  //価格が取得できなかった場合
           break;  //ループを抜ける
         }
-        
-        console.log(n+":OK");
-        price.map((e,index)=>{   //価格を数値に変換
-      // console.log(rating[index]); //配列を出力
 
-      const split_rating = rating[index].split(/\r\n|\n/);  //評価数を分割
-          if(rating[index] == '-'){  //評価数がundefindedの場合
-            allData.push({title:title[index],url:url[index],price:price[index].replace('円',''),rating:'0'});  //配列に格納&評価数を0にする&不要な文字を削除
-          }else{
-            rating[index] = split_rating[0];  //分割した評価数を格納
-            // allData.push({title:title[index],price:price[index].replace('円',''),rating:split_rating[1].replace('(','').replace(')','').trim()});  //配列に格納&不要な文字を削除
-            if(split_rating[1] == undefined){
-              allData.push({title:title[index],url:url[index],price:price[index].replace('円',''),rating:'0'});  //配列に格納&不要な文字を削除
-            }else{
-            allData.push({title:title[index],url:url[index],price:price[index].replace('円',''),rating:split_rating[1].replace('(','').replace(')','').trim()});  //配列に格納&不要な文字を削除          }
-          
-          // allData.push({  //配列に格納
-          //   title:title[index],  // Github Compilotの提案でなぜかタイトル取れたすげぇ
-          //   rating:rating[index],
-          //   price:price[index]  //価格
-          // });
-            }
-        }
-      })
+        console.log(n + ":OK");
+        price.map((e, index) => {   //価格を数値に変換
+          // console.log(rating[index]); //配列を出力
+          if (rating[index] === undefined) {
+            rating[index] = "0";
+          } else {
+            rating[index] = rating[index].replace(/[^0-9]/g, ''); //数字以外を削除
+          }
+          allData.push({ title: title[index], url: url[index], price: price[index].replace('円', ''), rating: rating[index] });
+        })
+
         // console.log("================");
         n++;
-        
+
       }
       resolve();  //resolveでPromiseを終了させる
-  })
-}
-    await promise();  //promiseを実行
-    if(allData.length>0) {  //配列が空でない場合
-      console.log(allData); //配列を出力
-      csv.stringify(allData,(error,output)=>{ //CSVに変換
-        fs.writeFile(keyWord +'.csv',output,(error)=>{  //CSVファイルに書き込み
-            console.log("csvファイルの書き出しに成功");
-        })
     })
-    }
+  }
+  await promise();  //promiseを実行
+  if (allData.length > 0) {  //配列が空でない場合
+    console.log(allData); //配列を出力
+    csv.stringify(allData, (error, output) => { //CSVに変換
+      fs.writeFile(keyWord + '.csv', output, (error) => {  //CSVファイルに書き込み
+        console.log("csvファイルの書き出しに成功");
+      })
+    })
+  }
   await browser.close();  //ブラウザを閉じる
-  
+
 })();
--- test.js
+++ test.js
@@ -3,9 +3,9 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://coconala.com');
-
-  const datas = await page.$$eval('a', list => list.map(item => item.href));
+  await page.goto('https://coconala.com/search?keyword=web%20%E5%88%B6%E4%BD%9C&category_id=500');
+  const test = '.c-searchPage_itemList a';
+  const datas = await page.$$eval(test, list => list.map(item => item.href));
 
   console.log(datas);
 
